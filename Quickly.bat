af08daf413da8866ded1c1550907dda9fa407bf2
horizontti <77754465+horizontti@users.noreply.github.com>
2021-04-23 22:50:53 +0300

    Create Quickly.bat

---
+++ Quickly.bat
@@ -0,0 +1,4 @@
+Echo batch file delete folder
+@RD /S /Q "C:\Users\Administrator\Desktop\defaulrp\server\crashes"
+@RD /S /Q "C:\Users\Administrator\Desktop\defaulrp\server\cache"
+START C:\Users\Administrator\Desktop\defaulrp\start.bat
